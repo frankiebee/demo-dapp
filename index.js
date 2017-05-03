735ef7a44eca7aaa1e7b1fec84a6da311620087c
frankiebee <frankie.diamond@gmail.com>
2017-05-03 12:51:17 +0200

    Fix and make things "pretty"

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ function startApp () {
   const query = new EthQuery(provider)
   let account = web3.eth.accounts[0]
   button('getBalance', () => query.getBalance(account, handleResult))
+  button('getTransactionCount', () => query.getTransactionCount(account,handleResult))
   // TODOS:
-  // button('gasPrice', () => query.gasPrice(handleResult)
-  // button('getTransactionCount', () => query.(,handleResult)
-  // button('getStorageAt', () => query.(,handleResult)
-  // button('accounts', () => query.(,handleResult)
-  // button('blockNumber', () => query.(,handleResult)
-  // button('getBlockTransactionCountByHash', () => query.(,handleResult)
-  // button('getBlockTransactionCountByNumber', () => query.(,handleResult)
-  // button('sendTransaction', () => query.(,handleResult)
-  // button('estimateGas', () => query.(,handleResult)
-  // button('getBlockByHash', () => query.(,handleResult)
-  // button('getBlockByNumber', () => query.(,handleResult)
-  // button('getTransactionByHash', () => query.(,handleResult)
-  // button('getTransactionReceipt', () => query.(,handleResult)
+  // button('gasPrice', () => query.gasPrice(handleResult))
+  // button('getStorageAt', () => query.(,handleResult))
+  // button('accounts', () => query.(,handleResult))
+  // button('blockNumber', () => query.(,handleResult))
+  // button('getBlockTransactionCountByHash', () => query.(,handleResult))
+  // button('getBlockTransactionCountByNumber', () => query.(,handleResult))
+  // button('sendTransaction', () => query.(,handleResult))
+  // button('estimateGas', () => query.(,handleResult))
+  // button('getBlockByHash', () => query.(,handleResult))
+  // button('getBlockByNumber', () => query.(,handleResult))
+  // button('getTransactionByHash', () => query.(,handleResult))
+  // button('getTransactionReceipt', () => query.(,handleResult))
 
 }
 
